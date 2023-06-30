f8fbeecbfa3d2ca4fd9e67a341808f9b4c50fff1
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2023-06-30 15:29:14 -0400

    Image rendering nearly complete

--- app/WeatherCard.js
+++ app/WeatherCard.js
@@ -6,7 +6,10 @@ function WeatherCard(props) {
   return (
     <div className="text-center mx-20 flex items-center justify-center flex-col">
       <h2>{item.name}</h2>
-      <WeatherImage shortForecast={item.shortForecast} />
+      <WeatherImage
+        shortForecast={item.shortForecast}
+        isDaytime={item.isDaytime}
+      />
       <p className="my-5">{item.shortForecast}</p>
       <p>{item.temperature} F°</p>
       <p>{item.humidity}</p>
--- app/WeatherImage.js
+++ app/WeatherImage.js
@@ -1,13 +1,87 @@
 import Image from "next/image";
-import sun from "../media/forecast-icns/sun.png";
+import sunny from "../media/forecast-icns/sun.png";
+import sunrain from "../media/forecast-icns/sun_rain.png";
+import sunfog from "../media/forecast-icns/sun_fog.png";
+import sunclouds from "../media/forecast-icns/sun_clouds.png";
+import sunlighting from "../media/forecast-icns/sun_lighting.png";
+
+import moon from "../media/forecast-icns/moon.png";
+import moonrain from "../media/forecast-icns/moon_rain.png";
+import moonlighting from "../media/forecast-icns/moon_lighting.png";
+import moonfog from "../media/forecast-icns/moon_fog.png";
+import moonclouds from "../media/forecast-icns/moon_clouds.png";
 
 function WeatherImage(props) {
-  const shortForecast = props.shortForecast;
-  return (
-    <div>
-      <Image src={sun} alt="Sunshine"></Image>
-    </div>
-  );
+  const { shortForecast, isDaytime } = props;
+
+  const forecastWordArr = shortForecast.split(" ");
+  console.log(forecastWordArr);
+
+  if (isDaytime) {
+    if (forecastWordArr.includes("Thunderstorms")) {
+      return (
+        <div>
+          <Image src={sunlighting} alt="Thunderstorms"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Showers")) {
+      return (
+        <div>
+          <Image src={sunrain} alt="Showers"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Haze")) {
+      return (
+        <div>
+          <Image src={sunfog} alt="Haze"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Rain")) {
+      return (
+        <div>
+          <Image src={sunrain} alt="Rain"></Image>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Image src={sunny} alt="Sun"></Image>
+        </div>
+      );
+    }
+  } else {
+    if (forecastWordArr.includes("Thunderstorms")) {
+      return (
+        <div>
+          <Image src={moonlighting} alt="Thunderstorms"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Showers")) {
+      return (
+        <div>
+          <Image src={moonrain} alt="Showers"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Haze")) {
+      return (
+        <div>
+          <Image src={moonfog} alt="Haze"></Image>
+        </div>
+      );
+    } else if (forecastWordArr.includes("Rain")) {
+      return (
+        <div>
+          <Image src={moonrain} alt="Rain"></Image>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Image src={moon} alt="Moon"></Image>
+        </div>
+      );
+    }
+  }
 }
 
 export default WeatherImage;
